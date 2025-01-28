3197016998379006502b7c0f0800f07c87a3ccf3
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-01-27 22:51:19 -0300

    feat: created Brand model

--- cars/cars/models.py
+++ cars/cars/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return super().__str__()
+
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
-    brand = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name="car_brand")
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
 
     def __str__(self) -> str:
-        return f"{self.model} - {self.brand}"
+        return self.model
+    
