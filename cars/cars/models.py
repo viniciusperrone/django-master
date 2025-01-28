08286f8c892c2bc017d698bc8e42e31d8bdfcf9d
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-01-27 22:55:05 -0300

    feat: created BrandAdmin and register Brand model in admin

--- cars/cars/models.py
+++ cars/cars/models.py
@@ -7,7 +7,7 @@ class Brand(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self) -> str:
-        return super().__str__()
+        return self.name
 
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
