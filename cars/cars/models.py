dbfaa6b412cd7386ada8a6b36e407d378f308296
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-01-28 00:10:11 -0300

    feat: added photo and plate fields in Car model

--- cars/cars/models.py
+++ cars/cars/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
@@ -15,7 +13,9 @@ class Car(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name="car_brand")
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
 
     def __str__(self) -> str:
         return self.model
