3e43536750e6063be4f4eaa179e83d15b50220c4
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-01-31 22:20:50 -0300

    feat: added cars.html

--- cars/cars/views.py
+++ cars/cars/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from cars.models import Car
 
-# Create your views here.
+def cars_view(request):
+    cars = Car.objects.all()
+    
+    return render(
+        request,
+        'cars.html',
+        { 'cars': cars }
+    )
