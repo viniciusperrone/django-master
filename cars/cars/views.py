a6d18bfe4c6330513d51ef36b0dad7ce3d7b7f95
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-01 19:31:05 -0300

    feat: added delete car

--- cars/cars/views.py
+++ cars/cars/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from cars.models import Car
 from cars.forms import CarModelForm
@@ -31,4 +31,9 @@ class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
     template_name = 'car_update.html'
+    success_url = '/cars/'
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
     success_url = '/cars/'
--- cars/core/urls.py
+++ cars/core/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from cars.views import CarsListView, NewCarCreateView, CarDetailView, CarUpdateView
+from cars.views import CarsListView, NewCarCreateView, CarDetailView, CarUpdateView, CarDeleteView
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
@@ -31,6 +31,6 @@ urlpatterns = [
     path('new_car/', NewCarCreateView.as_view(), name='new_car'),
     path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
     path('car/<int:pk>/update', CarUpdateView.as_view(), name='car_update'),
-    # path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
+    path('car/<int:pk>/delete', CarDeleteView.as_view(), name='car_delete'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
