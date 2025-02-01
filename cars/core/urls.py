8925497e074eea8212cd54ca130af44a0fa4585e
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-01 18:50:25 -0300

    feat: added detail car url

--- cars/core/urls.py
+++ cars/core/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from cars.views import CarsListView, NewCarCreateView
+from cars.views import CarsListView, NewCarCreateView, CarDetailView
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
@@ -29,4 +29,6 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('cars/', CarsListView.as_view(), name='cars_list'),
     path('new_car/', NewCarCreateView.as_view(), name='new_car'),
+    path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
