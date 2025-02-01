52b4d49b79ded0f11abb306b24c9f2fa6454b74e
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-01 14:16:37 -0300

    fix: ajusted authenticate parameters

--- cars/accounts/views.py
+++ cars/accounts/views.py
@@ -20,7 +20,7 @@ def login_view(request):
         username = request.POST["username"]
         password = request.POST["password"]
 
-        user = authenticate(request, username, password)
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
