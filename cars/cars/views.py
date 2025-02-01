a7f1f95448abb62990a0d5af036471fc2f9d63f2
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-01 18:41:53 -0300

    feat: created CarDetailView

--- cars/cars/views.py
+++ cars/cars/views.py
@@ -1,8 +1,4 @@
-from typing import Any
-from django.db.models.query import QuerySet
-from django.shortcuts import render, redirect
-from django.views import View
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
 from cars.models import Car
 from cars.forms import CarModelForm
@@ -26,3 +22,7 @@ class NewCarCreateView(CreateView):
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
