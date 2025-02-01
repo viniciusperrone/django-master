80b885b719ec38be3fcac3317b82fcb2df1e4f81
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-01 17:56:19 -0300

    refactor: remove NewCarView

--- cars/cars/views.py
+++ cars/cars/views.py
@@ -21,32 +21,6 @@ class CarsListView(ListView):
         
         return cars
     
-
-class NewCarView(View):
-    
-    def get(self, request):
-        new_car_form = CarModelForm()
-        
-        return render(
-            request,
-            'new_car.html',
-            { 'new_car_form': new_car_form }
-        )
-
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-
-        if new_car_form.is_valid():
-            new_car_form.save()
-
-            return redirect('cars_list')
-
-        return render(
-            request,
-            'new_car.html',
-            { 'new_car_form': new_car_form }
-        )
-    
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
