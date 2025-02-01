bbc98016c8f301bccfd4fc7b488374aa0c42296f
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-01 10:19:00 -0300

    fix: improving forms

--- cars/cars/forms.py
+++ cars/cars/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from cars.models import Brand
+from cars.models import Brand, Car
 
 
 class CarForm(forms.Form):
@@ -10,3 +10,18 @@ class CarForm(forms.Form):
     plate = forms.CharField(max_length=10)
     value = forms.FloatField()
     photo = forms.ImageField()
+
+    def save(self):
+        car = Car(
+            model=self.cleaned_data['model'],
+            brand=self.cleaned_data['brand'],
+            factory_year=self.cleaned_data['factory_year'],
+            model_year=self.cleaned_data['model_year'],
+            plate=self.cleaned_data['plate'],
+            value=self.cleaned_data['value'],
+            photo=self.cleaned_data['photo'],
+        )
+
+        car.save()
+
+        return car
