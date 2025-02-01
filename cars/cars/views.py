da561e6ef3ea3fb6987db3c6c8141f85b22adec9
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-01-31 22:58:28 -0300

    fix: added order_by in cars

--- cars/cars/views.py
+++ cars/cars/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('model')
     search = request.GET.get('search')
 
     if search:
