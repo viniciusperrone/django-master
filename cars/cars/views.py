987d55e39882131b7e090d34003730ee3251ad0d
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-01-31 22:56:38 -0300

    feat: added handle parameters in cars_view

--- cars/cars/views.py
+++ cars/cars/views.py
@@ -3,6 +3,10 @@ from cars.models import Car
 
 def cars_view(request):
     cars = Car.objects.all()
+    search = request.GET.get('search')
+
+    if search:
+        cars = cars.filter(model__contains=search)
     
     return render(
         request,
