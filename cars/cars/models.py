ab153b634a5de3a3f3ac3b92fbb44936528e8387
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-03 19:16:20 -0300

    feat: created CarInventory

--- cars/cars/models.py
+++ cars/cars/models.py
@@ -19,4 +19,14 @@ class Car(models.Model):
 
     def __str__(self) -> str:
         return self.model
-    
+
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.cars_count} - {self.cars_value}"
