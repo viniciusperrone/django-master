2a4bbaef5e3409ba4ea8420c7ad6e954c1aefd68
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-03 19:26:53 -0300

    feat: improving car_post_save

--- cars/cars/signals.py
+++ cars/cars/signals.py
@@ -1,19 +1,20 @@
-from django.db.models.signals import pre_save, pre_delete, post_save, post_delete
+from django.db.models.signals import post_save, post_delete
+from django.db.models import Sum
 from django.dispatch import receiver
 
-from cars.models import Car
-
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    print("### PRE SAVE ###")
+from cars.models import Car, CarInventory
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
-    print("### POST SAVE ###")
+    cars_count = Car.objects.all().count()
+    cars_value = Car.objects.aaggregate(
+        total_value=Sum('value')
+    )['total_value']
 
-@receiver(pre_delete, sender=Car)
-def car_pre_delete(sender, instance, **kwargs):
-    print("### PRE DELETE ###")
+    CarInventory.objects.create(
+        cars_count=cars_count
+        cars_value=cars_value
+    )
 
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
