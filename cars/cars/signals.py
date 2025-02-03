758dac58aede1a10f84e94d91a7f3c0675bed378
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-03 19:29:00 -0300

    feat: improving signals.py

--- cars/cars/signals.py
+++ cars/cars/signals.py
@@ -4,18 +4,21 @@ from django.dispatch import receiver
 
 from cars.models import Car, CarInventory
 
-@receiver(post_save, sender=Car)
-def car_post_save(sender, instance, **kwargs):
+def car_inventory_update():
     cars_count = Car.objects.all().count()
     cars_value = Car.objects.aaggregate(
         total_value=Sum('value')
     )['total_value']
 
     CarInventory.objects.create(
-        cars_count=cars_count
+        cars_count=cars_count,
         cars_value=cars_value
     )
 
+@receiver(post_save, sender=Car)
+def car_post_save(sender, instance, **kwargs):
+    car_inventory_update()
+
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
-    print("### POST DELETE ###")
+    car_inventory_update()
