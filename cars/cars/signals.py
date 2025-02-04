e901d5aff77eb40691d3e708b96e76999d01823f
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-03 21:03:44 -0300

    feat: implement gemini ai in car_pre_save

--- cars/cars/signals.py
+++ cars/cars/signals.py
@@ -1,8 +1,24 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.db.models import Sum
 from django.dispatch import receiver
+from django.conf import settings
 
 from cars.models import Car, CarInventory
+
+import google.generativeai as genai 
+
+genai.configure(api_key=settings.API_KEY) 
+
+def car_gemini_ai(model, brand, year): 
+    message = ''' Elaborar um resumo sobre o carro {} da marca {} do ano {} com 200 caracteres. ''' 
+    message = message.format(model, brand, year) 
+    model = genai.GenerativeModel("gemini-1.5-flash") 
+    response = model.generate_content( 
+        message, 
+        generation_config = genai.GenerationConfig(max_output_tokens=300,) 
+    ) 
+    
+    return response.text
 
 def car_inventory_update():
     cars_count = Car.objects.all().count()
@@ -14,6 +30,14 @@ def car_inventory_update():
         cars_count=cars_count,
         cars_value=cars_value
     )
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.bio:
+        instance.bio = car_gemini_ai(instance.model, instance.brand, instance.model_year)
+        
+        print(instance.bio)
+
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
--- cars/core/settings.py
+++ cars/core/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+API_KEY = os.getenv("API_KEY")
