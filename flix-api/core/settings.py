5ca225db1047309bb2ae4eca2c04f5bc7e89e15f
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-16 18:24:38 -0300

    feat: integrated app with pg db

--- flix-api/core/settings.py
+++ flix-api/core/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "db",
+        "PORT": "5432",
+        "USER": "myuser",
+        "PASSWORD": "mypassword",
+        "NAME": "core"
     }
 }
 
