19158b54b271f3bf426be1229f83537c70a18ec1
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-16 18:50:15 -0300

    feat: created genre_view

--- flix-api/core/urls.py
+++ flix-api/core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from genres.views import genre_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('genres/', genre_view, name='genre-list')
 ]
--- flix-api/genres/views.py
+++ flix-api/genres/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from genres.models import Genre
+
+def genre_view(request):
+    genres = Genre.objects.all()
+
+    data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+
+    return JsonResponse(data, safe=False)
