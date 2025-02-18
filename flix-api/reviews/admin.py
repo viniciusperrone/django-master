de8596f9244c334822d431a1278a00aa44e28e72
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-17 22:07:43 -0300

    feat: added reviews admin

--- flix-api/reviews/admin.py
+++ flix-api/reviews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import Review
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'movie', 'stars', 'comment')
