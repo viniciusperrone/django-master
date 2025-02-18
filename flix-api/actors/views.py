c812aaf54e9a70013149a9295e4075fa53b096b7
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-17 21:08:34 -0300

    feat: implement views in actors

--- flix-api/actors/views.py
+++ flix-api/actors/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from actors.models import Actor
+from actors.serializers import ActorSerializer
 
-# Create your views here.
+class ActorListCreateView(generics.ListCreateAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
--- flix-api/core/urls.py
+++ flix-api/core/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path
 
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
+from actors.views import ActorListCreateView, ActorRetrieveUpdateDestroyView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
-    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-list')
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail'),
+    path('actors/', ActorListCreateView.as_view(), name='actor-create-list'),
+    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail')
 ]
