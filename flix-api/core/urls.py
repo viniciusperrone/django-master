a172662231b795a682611d8b54adb42fcbaa53c5
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-17 21:32:02 -0300

    feat: implemented views of movies

--- flix-api/core/urls.py
+++ flix-api/core/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 from actors.views import ActorListCreateView, ActorRetrieveUpdateDestroyView
-
+from movies.views import MovieListCreateView, MovieRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
     path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail'),
     path('actors/', ActorListCreateView.as_view(), name='actor-create-list'),
-    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail')
+    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail'),
+    path('movies/', MovieListCreateView.as_view(), name='movie-create-list'),
+    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail')
 ]
--- flix-api/movies/views.py
+++ flix-api/movies/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from movies.models import Movie
+from movies.serializers import MovieSerializer
+
+class MovieListCreateView(generics.ListCreateAPIView):
+    queryset = Movie
+    serializer_class = MovieSerializer
+
+class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie
+    serializer_class = MovieSerializer
