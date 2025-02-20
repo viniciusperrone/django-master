cf55e1d30b81dc30a966d89f5b7e87a1389cf57a
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-19 23:30:20 -0300

    feat: get_rate method in MovieSerializer

--- flix-api/movies/serializers.py
+++ flix-api/movies/serializers.py
@@ -3,7 +3,24 @@ from rest_framework import serializers
 from movies.models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
+
+
+    def get_rate(self, obj):
+        reviews = obj.reviews.all()
+
+        if reviews:
+            sum_reviews = 0
+
+            for review in reviews:
+                sum_reviews += review.stars
+
+            reviews_count = reviews.count()
+
+            return round(sum_reviews / reviews_count, 1)
+        
+        return None
