b691900c5c150ba52204c955fa45cc1049158c6f
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-20 21:36:16 -0300

    fix: adjusted ReviewSerializer

--- flix-api/reviews/serializers.py
+++ flix-api/reviews/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
-from reviews.models import Movie
+from reviews.models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Movie
+        model = Review
         fields = '__all__'
