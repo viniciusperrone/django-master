bcffbb4163e45eb5766ab010ad253d3837909a96
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-21 23:03:07 -0300

    feat: protected views if user is not authenticated

--- flix-api/movies/views.py
+++ flix-api/movies/views.py
@@ -1,12 +1,15 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from movies.models import Movie
 from movies.serializers import MovieSerializer
 
 class MovieListCreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
