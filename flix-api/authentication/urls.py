1a08037d7c3bf68947780158c31d0513bc3e0fac
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-21 23:11:59 -0300

    fix: adjusted urls of authentication

--- flix-api/authentication/urls.py
+++ flix-api/authentication/urls.py
@@ -3,6 +3,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('authentication/token', TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify')
+    path('authentication/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('authentication/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
