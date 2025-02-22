ae16b754b0856fce28239ad8a32844fcf93fbadd
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-21 23:05:31 -0300

    fix: adjusted name path of token_verify

--- flix-api/authentication/urls.py
+++ flix-api/authentication/urls.py
@@ -4,5 +4,5 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('authentication/token', TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
