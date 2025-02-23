f68bc3e392705ece045cef4d67f4f533adf55605
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-22 23:18:48 -0300

    feat: improving GenrePermissionClass

--- flix-api/genres/views.py
+++ flix-api/genres/views.py
@@ -3,9 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 
 from genres.models import Genre
 from genres.serializers import GenreSerializer
+from genres.permissions import GenrePermissionClass
 
 class GenreCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GenrePermissionClass)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
