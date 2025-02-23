0a9bf4ea2625b5466d065cda73f46c9809617012
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-23 00:26:27 -0300

    chore: updated minuts of access token

--- flix-api/core/settings.py
+++ flix-api/core/settings.py
@@ -145,6 +145,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
