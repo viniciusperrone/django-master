9262c6013c684e2b7f8c320127726eeeef5bc8a8
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-23 00:19:32 -0300

    feat: applied GlobalDefaulPermission in all views

--- flix-api/movies/views.py
+++ flix-api/movies/views.py
@@ -4,12 +4,14 @@ from rest_framework.permissions import IsAuthenticated
 from movies.models import Movie
 from movies.serializers import MovieSerializer
 
+from utils.permissions import GlobalDefaultPermission
+
 class MovieListCreateView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
--- flix-api/reviews/views.py
+++ flix-api/reviews/views.py
@@ -4,13 +4,15 @@ from reviews.models import Review
 from reviews.serializers import ReviewSerializer
 from rest_framework.permissions import IsAuthenticated
 
+from utils.permissions import GlobalDefaultPermission
+
 class ReviewListCreateView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Review
     serializer_class = ReviewSerializer
 
 class ReviewRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
