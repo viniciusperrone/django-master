7f29ae84610fb3f83ec8746b645bf8e34f226e81
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-23 22:13:10 -0300

    feat: created MovieStatsView

--- flix-api/movies/urls.py
+++ flix-api/movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from movies.views import MovieListCreateView, MovieRetrieveUpdateDestroyView
+from movies.views import MovieListCreateView, MovieRetrieveUpdateDestroyView, MovieStatsView
 
 urlpatterns = [
     path('movies/', MovieListCreateView.as_view(), name='movie-create-list'),
     path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail'),
+    path('movies/stats/', MovieStatsView.as_view(), name='movie-stats'),
 ]
--- flix-api/movies/views.py
+++ flix-api/movies/views.py
@@ -1,8 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, views, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from django.db.models import Count, Avg
 
 from movies.models import Movie
 from movies.serializers import MovieSerializer
+from reviews.models import Review
 
 from utils.permissions import GlobalDefaultPermission
 
@@ -15,3 +19,25 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
+
+
+    def get(self, request):
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        total_reviews = Review.objects.count()
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+
+        return Response(
+            data={
+                'total_movies': total_movies,
+                'movies_by_genre': movies_by_genre,
+                'total_reviews': total_reviews,
+                'average_stars': average_stars
+            },
+            status=status.HTTP_200_OK
+        )
+    
