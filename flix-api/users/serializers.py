0c88f6bb1e82e77ed924c198c9328c25091804b9
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-28 23:34:49 -0300

    feat: include permission in user view

--- flix-api/users/serializers.py
+++ flix-api/users/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    permissions = serializers.SerializerMethodField()
+    
     class Meta:
         model = User
-        fields = ["id", "username", "email", "first_name", "last_name", "is_staff"]
+        fields = ["id", "username", "email", "first_name", "last_name", "is_staff", "permissions"]
+
+    def get_permissions(self, obj):
+        return obj.user_permissions.values_list("codename", flat=True)
+    
