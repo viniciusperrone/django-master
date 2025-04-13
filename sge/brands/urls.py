19600c306d49ee904f5033916b81cbccc4b602e4
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-13 18:57:53 -0300

    feat: added brand_delete template

--- sge/brands/urls.py
+++ sge/brands/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from brands.views import BrandListView, BrandCreateView, BrandDetailView, BrandUpdateView
+from brands.views import BrandListView, BrandCreateView, BrandDetailView, BrandUpdateView, BrandDeleteView
 
 
 urlpatterns = [
     path('brands/list', BrandListView.as_view(), name='brand_list'),
     path('brands/create/', BrandCreateView.as_view(), name="brand_create"),
     path('brands/<int:pk>/detail/', BrandDetailView.as_view(), name='brand_detail'),
-    path('brands/<int:pk>/update/', BrandUpdateView.as_view(), name='brand_update')
+    path('brands/<int:pk>/update/', BrandUpdateView.as_view(), name='brand_update'),
+    path('brands/<int:pk>/delete/', BrandDeleteView.as_view(), name='brand_delete')
 ]
--- sge/brands/views.py
+++ sge/brands/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from brands.models import Brand
 from brands.forms import BrandForm
@@ -34,4 +34,9 @@ class BrandUpdateView(UpdateView):
     model = Brand
     template_name = 'brand_update.html'
     form_class = BrandForm
+    success_url = reverse_lazy('brand_list')
+
+class BrandDeleteView(DeleteView):
+    model = Brand
+    template_name = 'brand_delete.html'
     success_url = reverse_lazy('brand_list')
