0330daa458e228aa97f5d30edef9b79883bc6aaa
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-13 17:12:03 -0300

    feat: added class in form and added anchor create brand in brand list

--- sge/brands/forms.py
+++ sge/brands/forms.py
@@ -8,3 +8,11 @@ class BrandForm(forms.ModelForm):
     class Meta:
         model = Brand
         fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'row': 3})
+        }
+        labels={
+            'name': 'Nome',
+            'description': 'Descrição'
+        }
