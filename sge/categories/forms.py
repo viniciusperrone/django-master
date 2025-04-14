ed9af4437812b315f86bb5fd5b5e29579fbc507a
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-14 10:13:43 -0300

    feat: created CategoryForm

---
+++ sge/categories/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from categories.models import Category
+
+
+class CategoryForm(forms.ModelForm):
+
+    class Meta:
+        model = Category
+        fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'row': 3})
+        }
+        labels={
+            'name': 'Nome',
+            'description': 'Descrição'
+        }
