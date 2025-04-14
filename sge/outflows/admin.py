8eee33d8227c911d6501ec8ed4786c0e2ecfb570
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-14 14:07:13 -0300

    refactor: renamed class name and renamed descriptions field with description

--- sge/outflows/admin.py
+++ sge/outflows/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-# Register your models here.
+# from outflows.models import Outflow
+
+
+# class OutflowAdmin(admin.ModelAdmin):
+#     list_display = ('supplier', 'product', 'quantity', 'created_at', 'updated_at')
+#     search_fields = ('supplier__name', 'product__title')
+
+# admin.site.register(Outflow, OutflowAdmin)
--- sge/outflows/models.py
+++ sge/outflows/models.py
@@ -3,11 +3,11 @@ from django.db import models
 from products.models import Product
 
 
-class OutFlow(models.Model):
+class Outflow(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='outflows')
 
     quantity = models.IntegerField()
-    descriptions = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
