8e1106cceb6ed70613294848ecfa5f55058bc1e8
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-15 14:52:43 -0300

    feat: added chart javascript library

--- sge/outflows/views.py
+++ sge/outflows/views.py
@@ -4,6 +4,8 @@ from django.views.generic import ListView, CreateView, DetailView
 from outflows.models import Outflow
 from outflows.forms import OutflowForm
 
+from utils.metrics import get_sales_metrics
+
 
 class OutflowListView(ListView):
     model = Outflow
@@ -19,6 +21,13 @@ class OutflowListView(ListView):
             queryset = queryset.filter(product__title__icontains=product)
 
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+
+        context_data["sales_metrics"] = get_sales_metrics()
+
+        return context_data
 
 class OutflowCreateView(CreateView):
     model = Outflow
