97106b28a3dd67c18f247f26c81de5e44da897ac
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-15 11:18:19 -0300

    chore: changed language code in settings

--- sge/core/metrics.py
+++ sge/core/metrics.py
@@ -1,3 +1,5 @@
+from django.utils.formats import number_format
+
 from typing import Dict
 from products.models import Product
 
@@ -11,10 +13,10 @@ def get_products_metrics() -> Dict:
     total_profit = total_selling_price - total_cost_price
 
     products_metrics = {
-        'total_cost_price': total_cost_price,
-        'total_selling_price': total_selling_price,
+        'total_cost_price': number_format(total_cost_price, decimal_pos=2, force_grouping=True),
+        'total_selling_price': number_format(total_selling_price, decimal_pos=2, force_grouping=True),
         'total_quantity': total_quantity,
-        'total_profit': total_profit
+        'total_profit': number_format(total_profit, decimal_pos=2, force_grouping=True)
     }
 
     return products_metrics
