3ba7be8b59997dad8bf1cc74874e323933da2755
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-17 09:37:48 -0300

    feat: added login required in brand views

--- sge/brands/views.py
+++ sge/brands/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
@@ -5,7 +6,7 @@ from brands.models import Brand
 from brands.forms import BrandForm
 
 
-class BrandListView(ListView):
+class BrandListView(LoginRequiredMixin, ListView):
     model = Brand
     template_name = 'brand_list.html'
     context_object_name = 'brands'
@@ -20,24 +21,24 @@ class BrandListView(ListView):
 
         return queryset
 
-class BrandCreateView(CreateView):
+class BrandCreateView(LoginRequiredMixin, CreateView):
     model = Brand
     template_name = 'brand_create.html'
     form_class = BrandForm
     success_url = reverse_lazy('brand_list')
 
 
-class BrandDetailView(DetailView):
+class BrandDetailView(LoginRequiredMixin, DetailView):
     model = Brand
     template_name = 'brand_detail.html'
 
-class BrandUpdateView(UpdateView):
+class BrandUpdateView(LoginRequiredMixin, UpdateView):
     model = Brand
     template_name = 'brand_update.html'
     form_class = BrandForm
     success_url = reverse_lazy('brand_list')
 
-class BrandDeleteView(DeleteView):
+class BrandDeleteView(LoginRequiredMixin, DeleteView):
     model = Brand
     template_name = 'brand_delete.html'
     success_url = reverse_lazy('brand_list')
