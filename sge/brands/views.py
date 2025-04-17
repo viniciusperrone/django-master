c74a45443a0d2da0254f0d329923b83f01fcbe58
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-17 10:31:28 -0300

    feat: added permissions in brands views

--- sge/brands/views.py
+++ sge/brands/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
@@ -6,11 +6,12 @@ from brands.models import Brand
 from brands.forms import BrandForm
 
 
-class BrandListView(LoginRequiredMixin, ListView):
+class BrandListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Brand
     template_name = 'brand_list.html'
     context_object_name = 'brands'
-    paginate_by = 1
+    paginate_by = 10
+    permission_required = 'brands.view_brand'
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -21,24 +22,28 @@ class BrandListView(LoginRequiredMixin, ListView):
 
         return queryset
 
-class BrandCreateView(LoginRequiredMixin, CreateView):
+class BrandCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Brand
     template_name = 'brand_create.html'
     form_class = BrandForm
     success_url = reverse_lazy('brand_list')
+    permission_required = 'brands.add_brand'
 
 
-class BrandDetailView(LoginRequiredMixin, DetailView):
+class BrandDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Brand
     template_name = 'brand_detail.html'
+    permission_required = 'brands.view_brand'
 
 class BrandUpdateView(LoginRequiredMixin, UpdateView):
     model = Brand
     template_name = 'brand_update.html'
     form_class = BrandForm
     success_url = reverse_lazy('brand_list')
+    permission_required = 'brands.change_brand'
 
 class BrandDeleteView(LoginRequiredMixin, DeleteView):
     model = Brand
     template_name = 'brand_delete.html'
     success_url = reverse_lazy('brand_list')
+    permission_required = 'brands.delete_brand'
