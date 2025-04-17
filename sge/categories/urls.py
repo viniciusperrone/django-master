f22a20cce06f3e973688390e2163997a5e430f3f
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-17 14:03:35 -0300

    feat: register categories on API

--- sge/categories/urls.py
+++ sge/categories/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from categories.views import CategoryListView, CategoryCreateView, CategoryDetailView, CategoryUpdateView, CategoryDeleteView
+from categories.views import (
+    CategoryListView, CategoryCreateView,
+    CategoryDetailView, CategoryUpdateView,
+    CategoryDeleteView, CategoryCreateListAPIView,
+    CategoryRetrieveUpdateDestroyAPIView
+)
+
+from utils.api import BASIC_API_URL
 
 
 urlpatterns = [
@@ -7,5 +14,8 @@ urlpatterns = [
     path('categories/create/', CategoryCreateView.as_view(), name='category_create'),
     path('categories/<int:pk>/detail/', CategoryDetailView.as_view(), name='category_detail'),
     path('categories/<int:pk>/update/', CategoryUpdateView.as_view(), name='category_update'),
-    path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category_delete')
+    path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category_delete'),
+
+    path(f'{BASIC_API_URL}/categories/', CategoryCreateListAPIView.as_view(), name='category-create-list-api-view'),
+    path(f'{BASIC_API_URL}/categories/<int:pk>/', CategoryRetrieveUpdateDestroyAPIView.as_view(), name='category-detail-api-view'),
 ]
--- sge/categories/views.py
+++ sge/categories/views.py
@@ -1,8 +1,10 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from categories.models import Category
+from categories.serializers import CategorySerializer
 from categories.forms import CategoryForm
 
 
@@ -47,3 +49,12 @@ class CategoryDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView
     template_name = 'category_delete.html'
     success_url = reverse_lazy('category_list')
     permission_required = 'categories.delete_category'
+
+class CategoryCreateListAPIView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
