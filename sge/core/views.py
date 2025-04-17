a173d5cb568668b64fb0c3e329ad3dfb09ff3f10
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-16 22:20:34 -0300

    feat: added get_daily_sales_quantity_data

--- sge/core/views.py
+++ sge/core/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 import json
 
-from utils.metrics import get_products_metrics, get_sales_metrics, get_daily_sales_data
+from utils.metrics import get_products_metrics, get_sales_metrics, get_daily_sales_data, get_daily_sales_quantity_data
+
 
 def home(request):
     products_metrics = get_products_metrics()
     sales_metrics = get_sales_metrics()
     daily_sales_data = get_daily_sales_data()
+    daily_sales_quantity_data = get_daily_sales_quantity_data()
 
     context = {
         'product_metrics': products_metrics,
         'sales_metrics': sales_metrics,
-        'daily_sales_data': json.dumps(daily_sales_data)
+        'daily_sales_data': json.dumps(daily_sales_data),
+        'daily_sales_quantity_data': json.dumps(daily_sales_quantity_data),
     }
 
     return render(request, 'home.html', context)
