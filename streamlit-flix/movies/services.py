ceeed2484256261f7010a9619061549c9e127096
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-25 16:31:33 -0300

    feat: created MovieService

---
+++ streamlit-flix/movies/services.py
@@ -0,0 +1,22 @@
+from movies.repository import MovieRepository
+
+
+class MovieService:
+
+    def __init__(self):
+        self.movie_repository = MovieRepository()
+
+
+    def get_movies(self):
+        return self.movie_repository.get_movies()
+
+    def create_movie(self, title, release_date, genre, actors, resume):
+        movie = dict(
+            title=title,
+            release_date=release_date,
+            genre=genre,
+            actors=actors,
+            resume=resume
+        )
+
+        return self.movie_repository.create_movie(movie)
