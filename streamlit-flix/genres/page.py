507712f6d49d8ad58280f36c2bf47f159e1041ac
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-25 13:07:20 -0300

    feat: improving page genres

--- streamlit-flix/genres/page.py
+++ streamlit-flix/genres/page.py
@@ -2,31 +2,26 @@ import pandas as pd
 
 import streamlit as st
 from st_aggrid import AgGrid
+from genres.services import GenreService
 
-
-genres = [
-    {
-        'id': 1,
-        'name': 'Ação'
-    },
-    {
-        'id': 2,
-        'name': 'Comédia'
-    },
-    {
-        'id': 3,
-        'name': 'Terror'
-    }
-]
 
 def show_genres():
-    st.write('Lista de Gêneros')
+    genre_services = GenreService()
+    genres = genre_services.get_genres()
 
-    AgGrid(
-        data=pd.DataFrame(genres),
-        reload_data=True,
-        key='genres_grid'
-    )
+    if genres:
+        st.write('Lista de Gêneros')
+
+        genres_df = pd.json_normalize(genres)
+
+        AgGrid(
+            data=genres_df,
+            reload_data=True,
+            key='genres_grid'
+        )
+
+    else:
+        st.warning('Nenhum gênero encontrado.')
 
     st.title('Cadastrar novo Gênero')
     name = st.text_input('Nome de Gênero')
