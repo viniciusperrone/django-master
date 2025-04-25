51ddb81d1cf9e1a7f53da7a5769be5a8a5668891
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-25 18:45:35 -0300

    feat: created ReviewRepository

---
+++ streamlit-flix/reviews/repository.py
@@ -0,0 +1,48 @@
+import requests
+
+import streamlit as st
+
+from login.service import logout
+
+
+class ReviewRepository:
+
+    def __init__(self) -> None:
+        self.__base_url = 'http://localhost:8000/api/v1'
+        self.__review_url = f'{self.__base_url}/review/'
+        self.__headers = {
+            'Authorization': f'Bearer {st.session_state.token}'
+        }
+
+    def get_reviews(self):
+        response = requests.get(
+            url=self.__review_url,
+            headers=self.__headers
+        )
+
+        if response.status_code == 200:
+            return response.json()
+
+        if response.status_code == 401:
+            logout()
+
+            return None
+
+        raise Exception(f'Erro ao obter dados da API. Status code: {response.status_code}')
+
+    def create_review(self, review):
+        response = requests.post(
+            url=self.__movies_url,
+            headers=self.__headers,
+            data=review
+        )
+
+        if response.status_code == 201 or response.status_code == 200:
+            return response.json()
+
+        if response.status_code == 401:
+            logout()
+
+            return None
+
+        raise Exception(f'Erro ao obter dados da API. Status code: {response.status_code}')
