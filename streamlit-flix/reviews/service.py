1694fabede1a97b9a6c3f09bff983668ca087bbe
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-25 19:32:03 -0300

    feat: created ReviewService

---
+++ streamlit-flix/reviews/service.py
@@ -0,0 +1,19 @@
+from reviews.repository import ReviewRepository
+
+
+class ReviewService:
+
+    def __init__(self):
+        self.review_repository = ReviewRepository()
+
+    def get_reviews(self):
+        return self.review_repository.get_reviews()
+
+    def create_review(self, movie, stars, comment):
+        review = dict(
+            movie=movie,
+            stars=stars,
+            comment=comment
+        )
+
+        return self.review_repository.create_review(review)
