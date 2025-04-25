f2cdf51487023eda08947750248b02f99c5f54d9
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-04-25 16:28:19 -0300

    feat: created MovieRepository

---
+++ streamlit-flix/movies/repository.py
@@ -0,0 +1,48 @@
+import requests
+
+import streamlit as st
+
+from login.service import logout
+
+
+class MovieRepository:
+
+    def __init__(self) -> None:
+        self.__base_url = 'http://localhost:8000/api/v1'
+        self.__movies_url = f'{self.__base_url}/movies/'
+        self.__headers = {
+            'Authorization': f'Bearer {st.session_state.token}'
+        }
+
+    def get_movies(self):
+        response = requests.get(
+            url=self.__movies_url,
+            headers=self.__headers
+        )
+
+        if response.status_code == 200:
+            return response.json()
+
+        if response.status_code == 401:
+            logout()
+
+            return None
+
+        raise Exception(f'Erro ao obter dados da API. Status code: {response.status_code}')
+
+    def create_movie(self, movie):
+        response = requests.post(
+            url=self.__movies_url,
+            headers=self.__headers,
+            data=movie
+        )
+
+        if response.status_code == 201 or response.status_code == 200:
+            return response.json()
+
+        if response.status_code == 401:
+            logout()
+
+            return None
+
+        raise Exception(f'Erro ao obter dados da API. Status code: {response.status_code}')
