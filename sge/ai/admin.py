5133a48e44e3103ce7affde01c2386da208e43db
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-14 19:46:54 -0300

    feat: registered AIResul model in admin

--- sge/ai/admin.py
+++ sge/ai/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from ai.models import AIResult
 
-# Register your models here.
+
+class AIResultAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'result')
+
+
+admin.site.register(AIResult, AIResultAdmin)
